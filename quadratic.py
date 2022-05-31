8adaaa3fa77788d56b4fc0eb73175ab13330b320
Shax <nedcas69@gmail.com>
2022-05-31 11:19:58 +0500

    ok!

--- quadratic.py
+++ quadratic.py
@@ -10,7 +10,7 @@ a = ''.join(a)
 b = ''.join(b)
 c = ''.join(c)
 
-if len(a) != 0 | len(b) != 0 | len(c) != 0:
+if  a.strip() or  b.strip() or  c.strip():
     a = float(a)
     b = float(b)
     c = float(c)
