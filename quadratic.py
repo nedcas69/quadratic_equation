c023be75648cc3756e734f11f2393874b386f794
Shax <nedcas69@gmail.com>
2022-06-01 08:48:10 +0500

    ok!

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,20 @@
-print('Решаем уравнение a•x²+b•x+c=0')
+print('Решаем уравнение a•x²+b•x+c=0') 
+#берем у пользователя значения
 s = input('Введите значение a: ')
 n = input('Введите значение b: ')
 v = input('Введите значение c: ')
+#Подключаем re
 import re
+# производим выборку цифр с помощью регулярных значений, она возврашает массив цифр  
 a = re.findall(r'\d+', s)
 b = re.findall(r'\d+', n)
 c = re.findall(r'\d+', v)
+#массив цифр переводим в строку
 a = ''.join(a)
 b = ''.join(b)
 c = ''.join(c)
-
-if  a.strip() or  b.strip() or  c.strip():
+# если значение строк не пустая переводим значения в float и решаем задачу
+if  a.strip() == 0  or  b.strip() == 0 or  c.strip() == 0:
     a = float(a)
     b = float(b)
     c = float(c)
@@ -39,4 +43,5 @@ if  a.strip() or  b.strip() or  c.strip():
             print('x₁ = ' + str(x1))
             print('x₂ = ' + str(x2))
 else:
+    #если в значениях нету цифр то печатаем значения как есть.
     print(str(s) + '•x²+' + str(n) + '•x+' + str(v) + '=0')
